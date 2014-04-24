051103e27007bbe0f7c6667d78e2133d2df65d9c
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-04-24 19:30:27 +0400

    Fixes

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -16,28 +16,4 @@ class FontAwesomeAsset extends AssetBundle
     public $css = array(
         'css/font-awesome.min.css',
     );
-
-    public $copyFolders = ['fonts', 'css'];
-
-    public function init()
-    {
-        $sourcePath = Yii::getAlias($this->sourcePath);
-        $this->publishOptions['beforeCopy'] = function ($from, $to) use ($sourcePath) {
-            if (strncmp(basename($from), '.', 1) === 0)
-                return false; // Do not copy hidden folders
-            else {
-                // Coping only css and fonts folders
-                foreach ($this->copyFolders as $folder) {
-                    $folder = $sourcePath . '/' . $folder;
-                    if (strpos($from, $folder) === 0)
-                        return true;
-                }
-                return false;
-            }
-        };
-
-        parent::init();
-    }
-
-
 }
